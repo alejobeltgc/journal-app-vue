44bd4717321432859549d0f6f4273004f055c911
Cristian GOnzalez <c.alejotgc@gmail.com>
2022-07-24 13:01:29 -0500

    Kickoff project

---
+++ src/pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <img src="/vite.svg" class="logo" alt="Vite logo" />
+  </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHashHistory } from "vue-router"
+
+
+const routes = [
+  { 
+    path: '/',
+    name: 'Home', 
+    component: () => import('../pages/Home.vue')
+  },
+  { 
+    path: '/about',
+    name: 'About', 
+    component: () => import('../pages/About.vue') 
+  },
+]
+
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes, 
+})
+
+
+export default router
